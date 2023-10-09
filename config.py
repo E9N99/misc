b2bfd459c68137b2b0c456691fd4eb26033852f8
BiLaL <97440505+E9N99@users.noreply.github.com>
2023-10-09 19:52:40 +0300

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Darksourcebot/X-MUSICC",
+    "https://github.com/E9N99/misc",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
